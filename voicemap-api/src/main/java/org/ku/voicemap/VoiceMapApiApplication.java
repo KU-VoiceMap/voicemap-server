fc068fc8c6135dcf9b9565dfe83bf43826a89832
donghoony <aru0504@naver.com>
2026-03-25 01:54:42 +0900

    배포준비

--- voicemap-api/src/main/java/org/ku/voicemap/VoiceMapApiApplication.java
+++ voicemap-api/src/main/java/org/ku/voicemap/VoiceMapApiApplication.java
@@ -1,12 +1,9 @@
 package org.ku.voicemap;
 
-import org.ku.voicemap.config.AiInstructionProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AiInstructionProperties.class)
 public class VoiceMapApiApplication {
 
     static void main(String[] args) {
---
+++ voicemap-api/src/main/java/org/ku/voicemap/config/AiInstructionConfig.java
@@ -0,0 +1,9 @@
+package org.ku.voicemap.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(AiInstructionProperties.class)
+public class AiInstructionConfig {
+}
